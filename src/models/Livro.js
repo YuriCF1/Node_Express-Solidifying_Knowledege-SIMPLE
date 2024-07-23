eb6c388b87e59ffc2c4e206ba1c52fb2a069a74a
Yuri Cruz <yuricruzf@gmail.com>
2024-07-23 14:42:31 -0300

    Adding custom erro messages

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,9 +6,25 @@ const livroSchema = new mongoose.Schema(
     id: { type: mongoose.Schema.Types.ObjectId },
     //   titulo: { type: mongoose.Schema.Types.String },
     titulo: { type: String, required: [true, "O nome do livro é obrigatório"] },
-    editora: { type: String },
+    editora: {
+      type: String,
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "A editora '{VALUE}' não é permitido.", //Personalizando a mensagem de erro. O mongoose entende o {VALUE} como o valor que foi enviado.
+      },
+    },
     preco: { type: Number },
-    paginas: { type: Number },
+    paginas: {
+      type: Number,
+      min: [
+        10,
+        "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+      ],
+      max: [
+        5000,
+        "O número de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}",
+      ],
+    },
     autor: autorSchema,
   },
   { versionKey: "_version1" }
