edfeacf13bbdb84ada68c81e4da43a93a915b6a2
Yuri Cruz <yuricruzf@gmail.com>
2024-07-23 14:49:28 -0300

    Creating global validation

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -16,14 +16,21 @@ const livroSchema = new mongoose.Schema(
     preco: { type: Number },
     paginas: {
       type: Number,
-      min: [
-        10,
-        "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
-      ],
-      max: [
-        5000,
-        "O número de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}",
-      ],
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000; //Fazendo validação personalizada
+        },
+        message:
+          "O número de páginas deve estar entre 10 e 5000. Valor fornecido '{VALUE}', não é aceito",
+      },
+      // min: [
+      //   10,
+      //   "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+      // ],
+      // max: [
+      //   5000,
+      //   "O número de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}",
+      // ],
     },
     autor: autorSchema,
   },
