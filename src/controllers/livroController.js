cc96dd4ce3e8a164885bb1ef0222e300b2397775
Yuri Cruz <yuricruzf@gmail.com>
2024-07-24 17:22:19 -0300

    Improving paging

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -6,22 +6,31 @@ class LivroController {
   static async listarLivros(req, res, next) {
     //static = Possibilita a chamada da função sem precisar criar um classe com 'new'
     try {
-      let { limite = 1, pagina = 1 } = req.query; //PAGINAÇÃO
+      console.log(req.query);
+      let {
+        limite = 1,
+        pagina = 1, //PAGINAÇÃO
+        // campoOrdenacao = "_id",
+        // ordem = -1,
+        ordenacao = "_id:-1",
+      } = req.query;
       // throw new Error();
-
-      limite - parseInt(limite);
-      pagina - parseInt(pagina);
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
+      
+      limite = parseInt(limite);
+      pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const listaLivros = await livro
           .find({}) //find = método mongoose. Não passei parâmetro então ele vai trazer tudo
+          // .sort({ _id: -1 }) //_id -> Campo que quero ordernar | -1 -> de forma decrescente, ou seja os adicionados mais recentes
+          .sort({ [campoOrdenacao]: ordem }) //titulo -> Campo que quero ordernar | - -> de forma crescente, neste caso em ordem alfabética
           .skip((pagina - 1) * limite) //Se tiver na pagina 1 -> 1-1 = 0 * 1 = 0 -> Não vai pular livro nenhum
           .limit(limite);
         res.status(200).json(listaLivros);
       } else {
-        next(
-          new RequisicaoIncorreta("Dados de paginação incorretos")
-        );
+        next(new RequisicaoIncorreta("Dados de paginação incorretos"));
       }
     } catch (error) {
       next(error);
